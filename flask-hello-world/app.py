77f865f9c451993fb7b3036415e3366c1cd3db54
Phinna Huang <hhh34@nau.edu>
2018-03-08 20:10:00 -0800

    create a hello-world app

---
+++ flask-hello-world/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+# create the application object
+app = Flask(__name__)
+
+# use the decorator pattern to
+# link the view function to a url
+@app.route("/")
+@app.route("/hello")
+
+# define the view using a function, which returns a string
+def hello_world():
+    return "Hello, World!"
+
+if __name__=="__main__":
+    app.run()
