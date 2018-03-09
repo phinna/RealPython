7a0d6baa8ca748ae140d492196d905909dfbe344
Phinna Huang <hhh34@nau.edu>
2018-03-09 15:24:26 -0800

    Add debug mode

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -3,6 +3,9 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use the decorator pattern to
 # link the view function to a url
 @app.route("/")
@@ -10,7 +13,32 @@ app = Flask(__name__)
 
 # define the view using a function, which returns a string
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!!!!"
 
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value+2
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print value+1
+    return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print value
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name), 200
+    else:
+        return "Hello, {}".format(name)
 if __name__=="__main__":
     app.run()
